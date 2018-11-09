94b754015d62e16043d889aa742219ea39025482
Toll123 <44909378+Toll123@users.noreply.github.com>
2018-11-09 22:34:28 +0100

    Formatiert und Gitignore erweitert

--- hal9000.js
+++ hal9000.js
@@ -8,7 +8,7 @@ var mirrors;
 
 fs.readFile('./mirrors.json', 'utf8', function (err, data) {
     if (err) {
-       console.log('error while reading file');
+        console.log('error while reading file');
     }
     mirrors = JSON.parse(data);
     //console.log(JSON.stringify(data, null, "\t"));
@@ -23,191 +23,187 @@ const rumtestenId = '381479229059104769';
 const rumtestenSpiegel = '381488016759455744';
 
 client.on("ready", () => {
-  console.log("I am ready!");
+    console.log("I am ready!");
 });
 
 client.on("message", (message) => {
-  mirrorMessageToMirrorChannels(message);
+    mirrorMessageToMirrorChannels(message);
 
-  if (message.content.startsWith("!:")) {
-    processCommands(message);
-  }
+    if (message.content.startsWith("!:")) {
+        processCommands(message);
+    }
 });
 
 function processCommands(message) {
-  if (!isAuthorAuthorized(message.author)) {
-    message.channel.send("**Denk nicht mal dran!**", { "reply" : message.author})
-    return;
-  }
-
-  if (message.content === "!:cleanse") {
-    if (isMessageChannelInOriginList(message.channel)) {
-      deleteStuff(message);
-    } else {
-      console.log("Channel not in origin list!");
-      message.channel.send("**Der Channel wird bisher nicht gespiegelt und kann nicht gesäubert werden!**");
+    if (!isAuthorAuthorized(message.author)) {
+        message.channel.send("**Denk nicht mal dran!**", {"reply": message.author})
+        return;
     }
-  }
 
-  if (message.content === "!:addMirror") {
-    if (isMessageChannelInOriginList(message.channel)) {
-      createMirrorForOrigin(message.channel);
-    } else {
-      console.log("Channel is not yet in origin list!");
-      message.channel.send("**Der Channel wird bisher nicht gespiegelt!**");
+    if (message.content === "!:cleanse") {
+        if (isMessageChannelInOriginList(message.channel)) {
+            deleteStuff(message);
+        } else {
+            console.log("Channel not in origin list!");
+            message.channel.send("**Der Channel wird bisher nicht gespiegelt und kann nicht gesäubert werden!**");
+        }
     }
-  }
 
-  if (message.content === "!:addOrigin") {
-    if (!isMessageChannelInOriginList(message.channel)) {
-      var originChannelId = createOrigin(message.channel);
+    if (message.content === "!:addMirror") {
+        if (isMessageChannelInOriginList(message.channel)) {
+            createMirrorForOrigin(message.channel);
+        } else {
+            console.log("Channel is not yet in origin list!");
+            message.channel.send("**Der Channel wird bisher nicht gespiegelt!**");
+        }
     }
-  }
 
-  if (message.content === "!:deleteMirror") {
-    deleteMessageChannelInOriginsMirrorList(message.channel)
-  }
+    if (message.content === "!:addOrigin") {
+        if (!isMessageChannelInOriginList(message.channel)) {
+            var originChannelId = createOrigin(message.channel);
+        }
+    }
+
+    if (message.content === "!:deleteMirror") {
+        deleteMessageChannelInOriginsMirrorList(message.channel)
+    }
 }
 
 function deleteMessageChannelInOriginsMirrorList(channel) {
-  var originList = mirrors.originList;
+    var originList = mirrors.originList;
 
-  for (var i = 0; i < originList.length; i++) {
-    var mirrorList = originList[i].mirrorList;
-    for (var j = 0; j < mirrorList.length; j++) {
-      if (mirrorList[j].id === channel.id) {
-        mirrorList.splice(j,1);
-        writeFile(mirrors, "mirrors.json");
-        channel.delete().catch(console.error);;
-      }
+    for (var i = 0; i < originList.length; i++) {
+        var mirrorList = originList[i].mirrorList;
+        for (var j = 0; j < mirrorList.length; j++) {
+            if (mirrorList[j].id === channel.id) {
+                mirrorList.splice(j, 1);
+                writeFile(mirrors, "mirrors.json");
+                channel.delete().catch(console.error);
+            }
+        }
     }
-  }
 }
 
 function createOrigin(channel) {
-  var originId = channel.id;
-  mirrors.originList.push({"id" : originId, "mirrorList" : []});
-  writeFile(mirrors, "mirrors.json");
+    var originId = channel.id;
+    mirrors.originList.push({"id": originId, "mirrorList": []});
+    writeFile(mirrors, "mirrors.json");
 }
 
 function createMirrorForOrigin(channel) {
-  var mirrorChannelName = channel.name + "_seit_" + getCurrentDate();
-  console.log(mirrorChannelName);
-  channel.guild.createChannel(mirrorChannelName, "text")
-    .then(newChannel => addMirrorToOrigin(channel, newChannel.id));
+    var mirrorChannelName = channel.name + "_seit_" + getCurrentDate();
+    console.log(mirrorChannelName);
+    channel.guild.createChannel(mirrorChannelName, "text")
+        .then(newChannel => addMirrorToOrigin(channel, newChannel.id));
 }
 
 function getCurrentDate() {
-  var date = new Date();
+    var date = new Date();
 
-  var year = date.getFullYear();
+    var year = date.getFullYear();
 
-  var month = date.getMonth() + 1;
-  month = (month < 10 ? "0" : "") + month;
+    var month = date.getMonth() + 1;
+    month = (month < 10 ? "0" : "") + month;
 
-  var day  = date.getDate();
-  day = (day < 10 ? "0" : "") + day;
+    var day = date.getDate();
+    day = (day < 10 ? "0" : "") + day;
 
-  return day + month + year;
+    return day + month + year;
 }
 
 function addMirrorToOrigin(channel, mirrorChannelId) {
-  var foundOriginObject = mirrors.originList.filter(function(originItem) {
-    return originItem.id == channel.id;
-  });
+    var foundOriginObject = mirrors.originList.filter(function (originItem) {
+        return originItem.id === channel.id;
+    });
 
-  foundOriginObject[0].mirrorList.push({"id" : mirrorChannelId});
+    foundOriginObject[0].mirrorList.push({"id": mirrorChannelId});
 
-  writeFile(mirrors, "mirrors.json");
+    writeFile(mirrors, "mirrors.json");
 }
 
 function writeFile(object, filename) {
-  fs.writeFile(filename, JSON.stringify(object, null, "\t"), (err) => {  
-    if (err) throw err;
-    console.log('Data written to file');
-  });
+    fs.writeFile(filename, JSON.stringify(object, null, "\t"), (err) => {
+        if (err) throw err;
+        console.log('Data written to file');
+    });
 }
 
 function isAuthorAuthorized(author) {
-  if (author.id === myId) {
-    return true;
-  } else {
-    return false;
-  }
+    return author.id === myId;
 }
 
 function mirrorMessageToMirrorChannels(message) {
-  if (isMessageChannelInOriginList(message.channel)) {
-    var originalAuthor = '**' + message.author.username + ':** ';
-    var mirrorMessage = originalAuthor + message.content;
-    
-    var originList = mirrors.originList;
+    if (isMessageChannelInOriginList(message.channel)) {
+        var originalAuthor = '**' + message.author.username + ':** ';
+        var mirrorMessage = originalAuthor + message.content;
 
-    var foundOriginObject = originList.filter(function(originItem) {
-      return originItem.id == message.channel.id;
-    });
+        var originList = mirrors.originList;
 
-    for (var i = 0; i < foundOriginObject[0].mirrorList.length; i++) {
-      var mirrorId = foundOriginObject[0].mirrorList[i].id;
-      var mirrorChannel = client.channels.find('id', mirrorId);
-      if (message.attachments.array().length == 0) {
-        mirrorChannel.send(mirrorMessage);
-      } else {
-        mirrorChannel.send(mirrorMessage, new Discord.Attachment(message.attachments.array()[0].url));
-      }
+        var foundOriginObject = originList.filter(function (originItem) {
+            return originItem.id === message.channel.id;
+        });
 
+        for (var i = 0; i < foundOriginObject[0].mirrorList.length; i++) {
+            var mirrorId = foundOriginObject[0].mirrorList[i].id;
+            var mirrorChannel = client.channels.find('id', mirrorId);
+            if (message.attachments.array().length === 0) {
+                mirrorChannel.send(mirrorMessage);
+            } else {
+                mirrorChannel.send(mirrorMessage, new Discord.Attachment(message.attachments.array()[0].url));
+            }
+
+        }
     }
-  }
 }
 
 function deleteStuff(msg) {
-  
-  let deleteStuff = () => {
-    let count = 0;
-    msg.channel.fetchMessages({limit: 100})
-     .then(messages => {
-       let messagesArr = messages.array();
-       let messageCount = messagesArr.length;
 
-       for(let i = 0; i < messageCount; i++) {
-         messagesArr[i].delete()
-          .then(function() {
-            count = count + 1;
-            console.log('geloescht!');
-            if(count >= 100) {
-              deleteStuff();
-              console.log('count >= 100!');
-            }
-          })
-          .catch(function() {
-            count = count + 1;
-            console.log('hab was gecatcht');
-            if(count >= 100) {
-              console.log('hab was gecatcht: count >= 100');
-              deleteStuff();
-            }
-          })
-       }
-     })
-     .catch(function(err) {
-       console.log('error thrown');
-       console.log(err);
-     });
-  };
+    let deleteStuff = () => {
+        let count = 0;
+        msg.channel.fetchMessages({limit: 100})
+            .then(messages => {
+                let messagesArr = messages.array();
+                let messageCount = messagesArr.length;
 
-  console.log('Versuche zu loeschen');
-  deleteStuff();
+                for (let i = 0; i < messageCount; i++) {
+                    messagesArr[i].delete()
+                        .then(function () {
+                            count = count + 1;
+                            console.log('geloescht!');
+                            if (count >= 100) {
+                                deleteStuff();
+                                console.log('count >= 100!');
+                            }
+                        })
+                        .catch(function () {
+                            count = count + 1;
+                            console.log('hab was gecatcht');
+                            if (count >= 100) {
+                                console.log('hab was gecatcht: count >= 100');
+                                deleteStuff();
+                            }
+                        })
+                }
+            })
+            .catch(function (err) {
+                console.log('error thrown');
+                console.log(err);
+            });
+    };
+
+    console.log('Versuche zu loeschen');
+    deleteStuff();
 }
 
 client.login(token);
 
 function isMessageChannelInOriginList(channel) {
-  var originList = mirrors.originList;
+    var originList = mirrors.originList;
 
-  for (var i = 0; i < originList.length; i++) {
-    if (originList[i].id === channel.id) {
-      return true;
+    for (var i = 0; i < originList.length; i++) {
+        if (originList[i].id === channel.id) {
+            return true;
+        }
     }
-  }
-  return false;
+    return false;
 }
--- presence.js
+++ presence.js
@@ -10,90 +10,94 @@ var lastOnline;
 
 fs.readFile('./lastonline.json', 'utf8', function (err, data) {
     if (err) {
-       console.log('error while reading file');
+        console.log('error while reading file');
     }
     lastOnline = JSON.parse(data);
 });
 
 
 client.on("ready", () => {
-  console.log("I am ready!");
+    console.log("I am ready!");
 
-  presenceChannel = client.channels.find('id', presenceChannelId);
+    presenceChannel = client.channels.find('id', presenceChannelId);
 });
 
 client.on("message", (message) => {
-  if (message.content === "?online") {
-    presenceChannel.send(JSON.stringify(lastOnline, null, "\t"));
-  }
+    if (message.content === "?online") {
+        presenceChannel.send(JSON.stringify(lastOnline, null, "\t"));
+    }
 });
 
 client.on("presenceUpdate", (oldMember, newMember) => {
-  logPresenceChange(newMember);
-  console.log("oldMember: " + oldMember.presence.status);
-  console.log("newMember: " + newMember.presence.status);
-  if (newMember.user.presence.status === "online" || oldMember.presence.status === "online") {
-    writeLastOnline(newMember);
-  }
+    logPresenceChange(newMember);
+    console.log("oldMember: " + oldMember.presence.status);
+    console.log("newMember: " + newMember.presence.status);
+    if (newMember.user.presence.status === "online" || oldMember.presence.status === "online") {
+        writeLastOnline(newMember);
+    }
 });
 
 function writeLastOnline(newMember) {
-  var foundMember = lastOnline.memberList.filter(function(member) {
-    return member.id == newMember.user.id;
-  });
+    var foundMember = lastOnline.memberList.filter(function (member) {
+        return member.id === newMember.user.id;
+    });
 
-  if (foundMember.length === 0) {
-    lastOnline.memberList.push({"id" : newMember.user.id, "userOrNickname" : getUserName(newMember), "lastOnline" : getCurrentTimestamp()});
-  } else {
-    foundMember[0].lastOnline = getCurrentTimestamp();
-  }
+    if (foundMember.length === 0) {
+        lastOnline.memberList.push({
+            "id": newMember.user.id,
+            "userOrNickname": getUserName(newMember),
+            "lastOnline": getCurrentTimestamp()
+        });
+    } else {
+        foundMember[0].lastOnline = getCurrentTimestamp();
+    }
 
-  writeFile(lastOnline, "lastonline.json");
+    writeFile(lastOnline, "lastonline.json");
 }
 
 function getCurrentTimestamp() {
-  var date = new Date();
-  date.setTime(date.getTime() - date.getTimezoneOffset()*60*1000);
-  return date.toJSON();
+    var date = new Date();
+    date.setTime(date.getTime() - date.getTimezoneOffset() * 60 * 1000);
+    return date.toJSON();
 }
 
 function logPresenceChange(newMember) {
-  var user = getUserName(newMember);
-  var germanPresenceMessage = extractGermanPresenceMessage(newMember.user.presence.status);
+    var user = getUserName(newMember);
+    var germanPresenceMessage = extractGermanPresenceMessage(newMember.user.presence.status);
 
-  var message = '**' + user + '** ' + germanPresenceMessage;
-  presenceChannel.send(message);
+    var message = '**' + user + '** ' + germanPresenceMessage;
+    presenceChannel.send(message);
 }
 
 function getUserName(newMember) {
-  if (newMember.nickname == null) {
-    return newMember.user.username;
-  } else {
-    return newMember.nickname;
-  }
+    if (newMember.nickname == null) {
+        return newMember.user.username;
+    } else {
+        return newMember.nickname;
+    }
 }
 
 function extractGermanPresenceMessage(status) {
-  switch(status) {
-    case "online":
-      return "ist nun online";
-    case "offline":
-      return "ist nun offline";
-    case "idle":
-      return "ist afk";
-    case "dnd":
-      return "will nicht gestört werden";
-    default:
-      console.error("this should not happen");
-      return "**das sollte nicht passieren**";
-  }
+    switch (status) {
+        case "online":
+            return "ist nun online";
+        case "offline":
+            return "ist nun offline";
+        case "idle":
+            return "ist afk";
+        case "dnd":
+            return "will nicht gestört werden";
+        default:
+            console.error("this should not happen");
+            return "**das sollte nicht passieren**";
+    }
 }
 
 client.login(token);
 
 function writeFile(object, filename) {
-  fs.writeFile(filename, JSON.stringify(object, null, "\t"), (err) => {  
-    if (err) throw err;
-    console.log('Data written to file');
-  });
+    fs.writeFile(filename, JSON.stringify(object, null, "\t"), (err) => {
+        if (err) throw err;
+        console.log('Data written to file');
+    });
 }
